42b7c30ad9b562c32ac82026672fa3001c1d38f5
Bertin19 <44229764+Bertin19@users.noreply.github.com>
2022-10-31 15:20:01 +0100

    Update HelloWorld.java
    changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String [] args) {
-		System.out.println("******** Welcome to my Jenkins Job ********");
+		System.out.println("******** Welcome to my Jenkins Job SCM changes ********");
 	}
 }
